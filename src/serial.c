5b57c735f9255b0a03359fd99f914e168e5f05dd
Marius Ebert <mebert1@luc.edu>
2021-04-20 17:56:34 -0500

    implement readLine

--- src/serial.c
+++ src/serial.c
@@ -1,5 +1,33 @@
 
-void putc(int data){
-	int *mu_io_reg = 0x3F215040;
+#include "uart.h"
+#include "serial.h"
+
+#define MU_IO_ADR 0x3F215040
+
+void putc(int data) {
+	unsigned int * mu_io_reg = (unsigned int *) MU_IO_ADR;
 	*mu_io_reg = data;
+}
+
+void readLine(char *input) {
+	int c_counter = 0;
+	clear_array(input, MAX_INPUT_SIZE);
+	char c;
+	// read until ENTER key is pressed
+	while((c = uart_getc()) != '\n') {
+		// print character to shell
+		uart_send(c);
+		input[c_counter] = c;
+		c_counter++;
+	}
+	uart_send(c);
+	// end line with linebreak
+	input[c_counter] = '\n';
+}
+
+// clears array of given length
+void clear_array(char *array, int array_size) {
+	for(int i = 0; i < array_size; i++) {
+		array[i] = 0;
+	}
 }
--- src/serial.h
+++ src/serial.h
@@ -1,1 +1,5 @@
+#define MAX_INPUT_SIZE 1000
+
 void putc(int data);
+void readLine(char *input);
+void clearArray(char *array, int size);
