3d04f130345bf8b5a32cdd04fc5be8027885523d
Marius Ebert <mebert1@luc.edu>
2021-05-04 16:56:03 -0500

    adjust process struct

--- src/process.h
+++ src/process.h
@@ -1,15 +1,16 @@
 #ifndef __PROCESS_H_
 #define __PROCESS_H_
 
+#include "page.h"
+
 struct process {
 	struct process *next;
 	struct process *prev;
 	uint32_t pid;
 	struct segment *segmentList; //linked list of segments belonging to this process. Each segment has a linked list of physical pages along with a virtual address. 
 		// Useful for context switching.
-	struct file_descriptor *open_file_list;
-	struct physical_page *L1table;
-	struct physical_page *L2table;
+	struct ppage *L1table;
+	struct ppage *L2table;
 	uint32_t syscall_params[8]; // used in the event that a syscall has to wait for the IO;
 	char path[16];
 };
